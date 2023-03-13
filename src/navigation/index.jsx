db3794f9dd17391114898c88f350a91d51a70c74
DavidPinilla <davidpinillame1995@gmail.com>
2023-03-12 20:47:34 -0500

    proyecto actualizado

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,14 +1,13 @@
 //importamos el NavigationContainer
 import { NavigationContainer } from "@react-navigation/native";
-import { useState } from "react";
 import TabsNavigator from "./tabs";
 import AuthNavigator from "./auth";
+import { useSelector } from "react-redux";
 
 //voy a declarar mi componente que le voy a llamar AppNavigator por que este va a ser el que va a contener toda la navegacion de mi App 
 
 const AppNavigator = () => {
-    //esto me va a retornar el NavigationContainer
-    const [userId, setUserId] = useState(null);
+    const userId = useSelector((state) => state.auth.userId);
     return <NavigationContainer>{userId ? <TabsNavigator /> : <AuthNavigator />}</NavigationContainer>
     };
     
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import {createStore, combineReducers, applyMiddleware } from "redux";
 import  thunk  from "redux-thunk";
-import { categoryReducer, productReducer, cartReducer, ordersReducer } from "./reducers";
+import { categoryReducer, productReducer, cartReducer, ordersReducer, authReducer } from "./reducers";
 
 const rootReducer = combineReducers({
     products: productReducer,
     category: categoryReducer,
     cart: cartReducer,
     orders: ordersReducer,
+    auth: authReducer,
 }); 
 
 
