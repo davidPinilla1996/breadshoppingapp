26678b0e6ae0c3e4c97e259b188f11201912413e
DavidPinilla <davidpinillame1995@gmail.com>
2023-03-13 12:30:58 -0500

    proyecto actualizado

--- src/store/actions/auth.action.js
+++ src/store/actions/auth.action.js
@@ -1,4 +1,4 @@
-import { URL_AUTH_SIGN_UP } from "../../constants/firebase";
+import { URL_AUTH_SIGN_IN, URL_AUTH_SIGN_UP } from "../../constants/firebase";
 import { auhtTypes } from "../types";
 
 const { SIGN_UP, SIGN_IN} = auhtTypes;
@@ -34,4 +34,29 @@ export const signUp = (email, password) => {
         } 
     };
 };
-export const signIn = (email, password) => {};
+export const signIn = (email, password) => {
+    return async (dispatch) => {
+        try {
+            const response = await fetch(URL_AUTH_SIGN_IN, {
+              method: 'POST',
+              header: {
+                'content-type': 'application/json',
+              },
+              body: JSON.stringify({
+                email,
+                password,
+                returnSecureToken: true,
+              }),
+            });
+
+            const data = await response.json();
+            dispatch({
+                type: SIGN_IN,
+                token: data.idToken,
+                userId: data.localId,
+            });
+        } catch (error) {
+            throw error;
+        }
+    };
+};
